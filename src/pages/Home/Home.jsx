15f4e4842d424cddc30dfc84bb7eaa48af818747
tajemouti <tajemouti@gmail.com>
2024-07-14 18:08:23 +0100

    Mount Header into the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Header from '../../components/Header/Header'
 import './home.css'
 
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Header />
     </div>
   )
 }
