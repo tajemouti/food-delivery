8e1e98ec64c822a2ec69c62d583c793820faa042
tajemouti <tajemouti@gmail.com>
2024-07-14 22:25:29 +0100

    Create store context provider

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { createContext } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+    const contextValue = {
+        food_list
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+StoreContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default StoreContextProvider;
+
