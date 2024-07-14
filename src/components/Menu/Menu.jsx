7d9cc599254979a9daf2ec422d7a10bbeb8b63cb
tajemouti <tajemouti@gmail.com>
2024-07-14 22:13:09 +0100

    Update Menu and Home components

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,22 +1,25 @@
 import './menu.css'
 import { menu_list } from '../../assets/assets'
 
-const Menu = () => {
+const Menu = ({ category, setCategory }) => {
   return (
     <div className='menu' id='menu'>
-        <h1>Explore our menu</h1>
-        <p className='menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients adn culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-        <div className="menu-list">
-            {menu_list.map((item, index)=>{
-                return (
-                    <div key={index} className="menu-list-item">
-                        <img src={item.menu_image} alt='image' />
-                        <p>{item.menu_name}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <hr />
+      <h1>Explore our menu</h1>
+      <p className='menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients adn culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <div className="menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)}
+              key={index} className="menu-list-item"
+            >
+              <img className={category===item.menu_name ? "active" : ""} src={item.menu_image} alt='image' />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr />
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import Header from '../../components/Header/Header'
 import Menu from '../../components/Menu/Menu'
 import './home.css'
 
 const Home = () => {
+  const [category, setCategory] = useState('All');
+
   return (
     <div className='home'>
       <Header />
-      <Menu />
+      <Menu category={category} setCategory={setCategory} />
     </div>
   )
 }
