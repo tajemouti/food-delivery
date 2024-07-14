8448710a65e9dfa03bb9489ee95e7f33188b06e0
tajemouti <tajemouti@gmail.com>
2024-07-14 15:36:42 +0100

    Add underline effect to navbar list elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import './navbar.css'
 import { assets } from '../../assets/assets'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState('home');
+
   return (
     <div className="navbar">
-        <img className='logo' src={assets.logo} alt="logo" />
-        <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile App</li>
-            <li>Contact Us</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="search" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="basket" />
-                <div className="dot"></div>
-            </div>
-            <button>Sign in</button>
+      <img className='logo' src={assets.logo} alt="logo" />
+      <ul className="navbar-menu">
+        <li onClick={()=> setMenu('home')} className={ menu==="home" ? "active" : "" }>Home</li>
+        <li onClick={()=> setMenu('menu')} className={ menu==="menu" ? "active" : "" }>Menu</li>
+        <li onClick={()=> setMenu('mobile-app')} className={ menu==="mobile-app" ? "active" : "" }>Mobile App</li>
+        <li onClick={()=> setMenu('contact-us')} className={ menu==="contact-us" ? "active" : "" }>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket" />
+          <div className="dot"></div>
         </div>
+        <button>Sign in</button>
+      </div>
     </div>
   )
 }
