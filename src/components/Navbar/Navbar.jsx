33abaa23d9f6510cbaeb87b612f814efe15b2613
tajemouti <tajemouti@gmail.com>
2024-07-14 14:09:59 +0100

    Define HTML structure for the Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import './navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+        <img className='logo' src={assets.logo} alt="logo" />
+        <ul className="navbar-menu">
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Mobile App</li>
+            <li>Contact Us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="search" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="basket" />
+                <div className="dot"></div>
+            </div>
+            <button>Sign in</button>
+        </div>
     </div>
   )
 }
