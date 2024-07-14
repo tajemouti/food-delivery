3d67e4405ef4d07c5a77c10c9298c77e82dfa34d
tajemouti <tajemouti@gmail.com>
2024-07-14 21:03:49 +0100

    Mount Menu compoent into the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header/Header'
+import Menu from '../../components/Menu/Menu'
 import './home.css'
 
 const Home = () => {
   return (
     <div className='home'>
       <Header />
+      <Menu />
     </div>
   )
 }
