34844b75796fb20733fd2204195c68859a0bdf06
tajemouti <tajemouti@gmail.com>
2024-07-15 23:07:14 +0100

    Create GetAPP component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header'
 import Menu from '../../components/Menu/Menu'
 import './home.css'
 import Food from '../../components/Food/Food'
+import GetApp from '../../components/GetApp/GetApp'
 
 const Home = () => {
   const [category, setCategory] = useState('All');
@@ -12,6 +13,7 @@ const Home = () => {
       <Header />
       <Menu category={category} setCategory={setCategory} />
       <Food category={category} />
+      <GetApp />
     </div>
   )
 }
