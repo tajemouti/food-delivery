00d942c72e697c9d2a184656fefefd9a5cb3c3ad
tajemouti <tajemouti@gmail.com>
2024-07-15 22:10:41 +0100

    Add a Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Order from './pages/Order/Order'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<Order />} />
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<Order />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import { assets } from '../../assets/assets'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla dolores laboriosam, provident animi vitae eum illum deleniti assumenda quis quibusdam sint inventore sit, ut repudiandae deserunt. Illum, nisi earum? Soluta?</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="facebook" />
+            <img src={assets.twitter_icon} alt="twitter" />
+            <img src={assets.linkedin_icon} alt="linkedin" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+212-617-708-648</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
