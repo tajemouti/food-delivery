674657e0db61b5949fb7005f444728568326639e
tajemouti <tajemouti@gmail.com>
2024-07-19 18:04:48 +0100

    Create Login component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Order from './pages/Order/Order'
 import Footer from './components/Footer/Footer'
+import Login from './components/Login/Login'
+import { useState } from 'react'
 
 const App = () => {
+  const [ showLogin, setShowLogin ] = useState(false);
+
   return (
     <>
+      {showLogin ? <Login /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
