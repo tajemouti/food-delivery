bafbbdfd481eaf336472891173ffdeec9bae83d7
tajemouti <tajemouti@gmail.com>
2024-07-20 17:25:40 +0100

    Link the cart icon to the cart page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,19 +9,23 @@ const Navbar = ({ setShowLogin }) => {
 
   return (
     <div className="navbar">
-      <img className='logo' src={assets.logo} alt="logo" />
+      <Link to='/'>
+        <img className='logo' src={assets.logo} alt="logo" />
+      </Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={()=> setMenu('home')} className={ menu==="home" ? "active" : "" }>Home</Link>
-        <a href='#menu' onClick={()=> setMenu('menu')} className={ menu==="menu" ? "active" : "" }>Menu</a>
-        <a href='#get-app' onClick={()=> setMenu('mobile-app')} className={ menu==="mobile-app" ? "active" : "" }>Mobile App</a>
-        <a href='#footer' onClick={()=> setMenu('contact-us')} className={ menu==="contact-us" ? "active" : "" }>Contact Us</a>
+        <Link to='/' onClick={() => setMenu('home')} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href='/#menu' onClick={() => setMenu('menu')} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href='/#get-app' onClick={() => setMenu('mobile-app')} className={menu === "mobile-app" ? "active" : ""}>Mobile App</a>
+        <a href='/#footer' onClick={() => setMenu('contact-us')} className={menu === "contact-us" ? "active" : ""}>Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search" />
-        <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="basket" />
-          <div className="dot"></div>
-        </div>
+        <Link to="/cart">
+          <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="basket" />
+            <div className="dot"></div>
+          </div>
+        </Link>
         <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
