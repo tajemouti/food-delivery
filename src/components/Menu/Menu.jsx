35288e3212ca344467d21cba55e27981fa6b3115
tajemouti <tajemouti@gmail.com>
2024-07-21 17:16:30 +0100

    Handle category inside redux store

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,31 +1,36 @@
-import PropTypes from 'prop-types';
-import './menu.css';
+import { useSelector, useDispatch } from 'react-redux';
 import { menuList } from '../../assets/assets';
+import { setCategory } from '../../redux/features/category/categorySlice';
+import './menu.css';
 
-const Menu = ({ category, setCategory }) => (
-  <div className="menu" id="menu">
-    <h1>Explore our menu</h1>
-    <p className="menu-text">Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients adn culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-    <div className="menu-list">
-      {menuList.map((item) => (
-        <button
-          onClick={() => setCategory((prev) => (prev === item.menu_name ? 'All' : item.menu_name))}
-          key={item.menu_name}
-          className="menu-list-item"
-          type="button"
-        >
-          <img className={category === item.menu_name ? 'active' : ''} src={item.menu_image} alt="menu item" />
-          <p>{item.menu_name}</p>
-        </button>
-      ))}
+const Menu = () => {
+  const category = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
+  const handleCategoryChange = (menuName) => {
+    dispatch(setCategory(menuName === category ? 'All' : menuName));
+  };
+
+  return (
+    <div className="menu" id="menu">
+      <h1>Explore our menu</h1>
+      <p className="menu-text">Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <div className="menu-list">
+        {menuList.map((item) => (
+          <button
+            onClick={() => handleCategoryChange(item.menu_name)}
+            key={item.menu_name}
+            className="menu-list-item"
+            type="button"
+          >
+            <img className={category === item.menu_name ? 'active' : ''} src={item.menu_image} alt="menu item" />
+            <p>{item.menu_name}</p>
+          </button>
+        ))}
+      </div>
+      <hr />
     </div>
-    <hr />
-  </div>
-);
-
-Menu.propTypes = {
-  category: PropTypes.string.isRequired,
-  setCategory: PropTypes.func.isRequired,
+  );
 };
 
 export default Menu;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import foodReducer from './features/food/foodSlice';
+import categoryReducer from './features/category/categorySlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    food: foodReducer,
+    category: categoryReducer,
   },
 });
 
